094a8a4f70f7884ccf49db52b87008dad0117cc9
Frances Fiona <115631823+fionalabrador@users.noreply.github.com>
2025-04-20 09:39:59 +0800

    login w/java (/)

--- app/src/main/java/com/example/pondmatev1/login.java
+++ app/src/main/java/com/example/pondmatev1/login.java
@@ -1,9 +1,11 @@
 package com.example.pondmatev1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +18,7 @@ public class login extends AppCompatActivity {
     EditText username;
     EditText password;
     Button loginButton;
+    TextView signupdirect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,24 @@ public class login extends AppCompatActivity {
 
         username = findViewById(R.id.username_input);
         password = findViewById(R.id.password_input);
+
         loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signupdirect = findViewById(R.id.signuptext);
+        signupdirect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, signup.class);
+                startActivity(intent);
+            }
+        });
         
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
