131b0ece042182a95e4aaf80d8a9d4cae9265544
PamelaJaneBabejes <pamelajanetopularbabejes@gmail.com>
2025-05-03 18:33:37 +0800

    user profile v1

--- app/src/main/java/com/example/pondmatev1/profile.java
+++ app/src/main/java/com/example/pondmatev1/profile.java
@@ -1,6 +1,10 @@
 package com.example.pondmatev1;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class profile extends AppCompatActivity {
@@ -8,5 +12,12 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
+
+        ImageButton backButton = findViewById(R.id.backbtn_profile);
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(profile.this, MainActivity.class);
+            startActivity(intent);
+            finish(); // Optional: closes the current activity
+        });
     }
 }
