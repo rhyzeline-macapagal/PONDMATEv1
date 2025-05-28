c6311cd14800c37731498ab906c9478e9e0b5733
Frances Fiona <115631823+fionalabrador@users.noreply.github.com>
2025-05-28 16:52:02 +0800

    INITIAL

--- app/src/main/java/com/example/pondmatev1/ControlsFeeder.java
+++ app/src/main/java/com/example/pondmatev1/ControlsFeeder.java
@@ -27,18 +27,16 @@ public class ControlsFeeder extends Fragment {
         View view = inflater.inflate(R.layout.controlsfeeder, container, false);
 
         // Set current date and time in PH timezone
-        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Manila"));
-        SimpleDateFormat dateTimeFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy - hh:mm a", Locale.ENGLISH);
-        dateTimeFormat.setTimeZone(TimeZone.getTimeZone("Asia/Manila"));
+        //Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Manila"));
+        //SimpleDateFormat dateTimeFormat = new SimpleDateFormat("EEEE, MMMM d, yyyy - hh:mm a", Locale.ENGLISH);
+        //dateTimeFormat.setTimeZone(TimeZone.getTimeZone("Asia/Manila"));
 
-        String currentDateTime = dateTimeFormat.format(calendar.getTime());
-        TextView textCurrentDate = view.findViewById(R.id.currentdate);
-        textCurrentDate.setText(currentDateTime);
+        //String currentDateTime = dateTimeFormat.format(calendar.getTime());
+        //TextView textCurrentDate = view.findViewById(R.id.currentdate);
+        //textCurrentDate.setText(currentDateTime);
 
         // Access fields if needed (already non-editable in XML)
-        TextView amtofFeeders = view.findViewById(R.id.amtoffeeders);
-        TextView lastFeedingTime = view.findViewById(R.id.lastfeedingtimedisplay);
-        TextView feedslevel = view.findViewById(R.id.feedlvldisplay);
+
 
         return view;
     }
